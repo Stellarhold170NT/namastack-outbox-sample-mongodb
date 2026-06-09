98f493d92b7a0ce5836c755487933c863ff02797
Huy Nguyễn Việt <huynv94@viettel.com.vn>
2026-06-09 08:15:09 +0700

    solution: upgrade to Namastack Outbox 1.6.0 official
    - Switch from io.github.stellarhold170NT fork to io.namastack official
    - Upgrade Namastack Outbox from 1.4.0 to 1.6.0
    - Update configuration to 1.6.x format (polling.fixed.interval, etc.)
    - Remove @EnableScheduling (auto-configured by Namastack 1.6.x)
    - Remove stale maven-local repository
    - Add explicit retry configuration

--- src/main/java/io/namastack/sample/SampleApplication.java
+++ src/main/java/io/namastack/sample/SampleApplication.java
@@ -2,13 +2,11 @@ package io.namastack.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableTransactionManagement
+// Note: @EnableScheduling is auto-configured by Namastack Outbox 1.6.x
 public class SampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
